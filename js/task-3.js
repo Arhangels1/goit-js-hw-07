fe6c0a63c84574edecbedf81835a2ae7c08d37fa
Arhangels1 <157172462+Arhangels1@users.noreply.github.com>
2024-04-19 20:54:52 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,6 @@ function checkForSpam(message) {
   let lowerCaseMessage = message.toLowerCase();
   return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
 }
-
 console.log(checkForSpam('Latest technology news')); // false
 console.log(checkForSpam('JavaScript weekly newsletter')); // false
 console.log(checkForSpam('Get best sale offers now!')); // true
