37030cd2d928580704ab3274d70c2d9a360f62bb
Arhangels1 <157172462+Arhangels1@users.noreply.github.com>
2024-06-08 22:42:03 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,34 +1,40 @@
-class Storage{
-  #items = [];
-
-  constructor(elements){
-    this.#items = elements;
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
   }
+];
 
-  getItems() {
-    return this.#items;
-  }
+const listElem = document.querySelector('ul')
 
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
-
-  removeItem(itemToRemove) {
-    if (this.#items.includes(itemToRemove)) {
-      const index = this.#items.indexOf(itemToRemove);
-      this.#items.splice(index, 1);
-    }
-  }
+function imageTemplate(img) {
+  return `<li><img class = "list-img" src="${img.url}" alt="${img.alt}"></li>`;
 }
 
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems());
+const markup = images.map(imageTemplate).join('');
 
-storage.addItem("Droid");
-console.log(storage.getItems());
+listElem.insertAdjacentHTML('afterbegin', markup);
 
-storage.removeItem("Prolonger");
-console.log(storage.getItems());
-
-storage.removeItem("Scaner");
-console.log(storage.getItems());
+const imagesElems = document.querySelectorAll('.list-img');
+imagesElems.forEach(img => img.style = 'width: 360px; height: 300px; object-fit: cover;')
+listElem.style = 'display: flex; gap: 50px; flex-wrap: wrap; list-style: none;';
